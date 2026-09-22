ec0b0864144b53615dc8c6a298bcbb58b8a8a125
Noza <odinaeva.nm@phystech.edu>
2026-09-22 17:44:20 +0300

    ещу один скрипт

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -1,8 +1,10 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time 
 GPIO.setmode(GPIO.BCM)
 led=26
+photo=6
 GPIO.setup(led, GPIO.OUT)
+GPIO.setup(photo, GPIO.IN)
 state=0
 period=1.0
 while True:
